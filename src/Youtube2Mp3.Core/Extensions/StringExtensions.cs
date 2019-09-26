b050a67dae5984ebdef8db74fb1054c72f32a47b
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-09-26 21:37:46 +0200

    Add RemoveIllegalPathCharacters extension method

---
+++ src/Youtube2Mp3.Core/Extensions/StringExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Youtube2Mp3.Core.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string RemoveIllegalPathCharacters(this string path)
+        {
+            string regexSearch = $"{Path.GetInvalidFileNameChars().ToString()}{Path.GetInvalidPathChars().ToString()}";
+            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
+            return r.Replace(path, "");
+        }
+    }
+}

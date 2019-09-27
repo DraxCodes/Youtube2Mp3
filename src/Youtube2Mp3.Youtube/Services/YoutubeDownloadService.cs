2748a678f73ba1cc0d4f160361ba59ddbfc2c599
draxcodes <joel@drax.codes>
2019-09-27 23:40:12 +0100

    Remove all references to YoutubeExplode in the YoutubeDownloadService

--- src/Youtube2Mp3.Youtube/Services/YoutubeDownloadService.cs
+++ src/Youtube2Mp3.Youtube/Services/YoutubeDownloadService.cs
@@ -2,21 +2,15 @@ using System.IO;
 using System.Threading.Tasks;
 using Youtube2Mp3.Core.Entities;
 using Youtube2Mp3.Core.Services;
-using Youtube2Mp3.Youtube.Helpers;
-using YoutubeExplode;
-using YoutubeExplode.Models;
-using YoutubeExplode.Models.MediaStreams;
 
 namespace Youtube2Mp3.Youtube.Services
 {
     public class YoutubeDownloadService : IDownloadService
     {
-        private readonly YoutubeClient _client;
         private readonly IStreamRepository _streamRepository;
 
-        public YoutubeDownloadService(YoutubeClient client, IStreamRepository streamRepository)
+        public YoutubeDownloadService(IStreamRepository streamRepository)
         {
-            _client = client;
             _streamRepository = streamRepository;
         }
 
@@ -26,14 +20,6 @@ namespace Youtube2Mp3.Youtube.Services
             var bytes = videoStream.ToArray();
 
             File.WriteAllBytes($"{filePath}/{track.Title}.mp3", bytes);
-        }
-
-        private async Task<Video> SearchYoutubeAsync(Track track)
-        {
-            var videos = await _client.SearchVideosAsync(track.Title);
-            var filteredResult = VideoFilter.BestMatchSingle(videos, track.Title, track.Duration);
-
-            return filteredResult;
         }
     }
 }

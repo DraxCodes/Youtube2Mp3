344d96cde5d26a7b246342fd4ed67f87a3dc509f
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-09-28 17:00:01 +0200

    Rename IReadOnlyList<Video> extensions

--- src/Youtube2Mp3.Youtube/Extensions/FilterExtensions.cs
+++ src/Youtube2Mp3.Youtube/Extensions/FilterExtensions.cs
@@ -7,17 +7,17 @@ namespace Youtube2Mp3.Youtube.Helpers
 {
     public static class VideoFilter
     {
-        public static Video ByTitleSingle(this IReadOnlyList<Video> videos, string title)
+        public static Video GetByTitle(this IReadOnlyList<Video> videos, string title)
         {
             return videos.FirstOrDefault(v => v.Title.Contains(title));
         }
 
-        public static IEnumerable<Video> ByTitleMany(this IReadOnlyList<Video> videos, string title)
+        public static IEnumerable<Video> GetManyByTitle(this IReadOnlyList<Video> videos, string title)
         {
             return videos.Where(v => v.Title == title);
         }
 
-        public static Video BestMatchSingle(this IReadOnlyList<Video> videos, string title, TimeSpan duration)
+        public static Video GetBestMatch(this IReadOnlyList<Video> videos, string title, TimeSpan duration)
         {
             var titleResults = videos.Where(v => v.Title == title);
             var durationResult = titleResults.FirstOrDefault(v => v.Duration.TotalSeconds == duration.TotalSeconds);
@@ -25,7 +25,7 @@ namespace Youtube2Mp3.Youtube.Helpers
             return durationResult;
         }
 
-        public static IEnumerable<Video> BestMatchMany(this IReadOnlyList<Video> videos, string title, TimeSpan duration)
+        public static IEnumerable<Video> GetBestMatches(this IReadOnlyList<Video> videos, string title, TimeSpan duration)
         {
             var titleResults = videos.Where(v => v.Title == title);
             var durationResults = titleResults.Where(v => v.Duration.TotalSeconds == duration.TotalSeconds);

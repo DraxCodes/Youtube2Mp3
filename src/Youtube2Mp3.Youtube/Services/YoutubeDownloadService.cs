981d360638a0850aee8473fd77747b7031b9a42b
Joel Parkinson <joelparkinson@live.co.uk>
2019-09-28 02:06:57 +0100

    Refactor Stream Downloading
    Fixes #11
    
    The issue in #11 was purely related to a .NetCore bug when specifically on Linux machines.
    Downloading of streams is now handled by us again rather than YoutubeExplode.

--- src/Youtube2Mp3.Youtube/Services/YoutubeDownloadService.cs
+++ src/Youtube2Mp3.Youtube/Services/YoutubeDownloadService.cs
@@ -2,42 +2,33 @@ using System.IO;
 using System.Threading.Tasks;
 using Youtube2Mp3.Core.Entities;
 using Youtube2Mp3.Core.Services;
-using Youtube2Mp3.Youtube.Helpers;
-using YoutubeExplode;
-using YoutubeExplode.Models;
-using YoutubeExplode.Models.MediaStreams;
 
 namespace Youtube2Mp3.Youtube.Services
 {
     public class YoutubeDownloadService : IDownloadService
     {
-        private readonly YoutubeClient _client;
+        private readonly IStreamRepository _streamRepository;
 
-        public YoutubeDownloadService(YoutubeClient client)
+        public YoutubeDownloadService(IStreamRepository streamRepository)
         {
-            _client = client;
+            _streamRepository = streamRepository;
         }
 
         public async Task DownloadMediaAsync(Track track, string filePath)
         {
-            var stream = new MemoryStream();
-            var video = await SearchYoutubeAsync(track);
-            var trackId = Helper.GetVideoId(video);
+            EnsurePathExists(filePath);
 
-            var streamInfoSet = await _client.GetVideoMediaStreamInfosAsync(trackId);
-            var audioStreamInfo = streamInfoSet.Audio.WithHighestBitrate();
+            var stream = await _streamRepository.GetStreamOfTrackAsync(track);
+            var streamArray = stream.ToArray();
 
-            var ext = audioStreamInfo.Container.GetFileExtension();
-
-            await _client.DownloadMediaStreamAsync(audioStreamInfo, $"{track.Title}.{ext}");
+            File.WriteAllBytes($"{filePath}/{track.Title}.mp3", streamArray);
         }
 
-        private async Task<Video> SearchYoutubeAsync(Track track)
+        private void EnsurePathExists(string filePath)
         {
-            var videos = await _client.SearchVideosAsync(track.Title, 1);
-            var filteredResult = VideoFilter.ByTitleSingle(videos, track.Title);
+            if (Directory.Exists(filePath)) { return; }
 
-            return filteredResult;
+            Directory.CreateDirectory(filePath);
         }
     }
 }

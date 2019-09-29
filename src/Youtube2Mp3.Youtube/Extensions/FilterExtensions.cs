12c5d21f7b6d87208d94ace603aa03eeddd2db22
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-09-29 16:26:28 +0200

    Add Extensions Add multiple extensions to IEnumerable<Video>, under which: GetByClosestTime, GetByArtists & refactor GetBestMatch

--- src/Youtube2Mp3.Youtube/Extensions/FilterExtensions.cs
+++ src/Youtube2Mp3.Youtube/Extensions/FilterExtensions.cs
@@ -7,30 +7,63 @@ namespace Youtube2Mp3.Youtube.Helpers
 {
     public static class VideoFilter
     {
-        public static Video GetByTitle(this IReadOnlyList<Video> videos, string title)
+        public static Video GetByTitle(this IEnumerable<Video> videos, string title)
+            => videos?.GetManyByTitle(title).FirstOrDefault();
+
+        public static IEnumerable<Video> GetManyByTitle(this IEnumerable<Video> videos, string title)
+            => videos?.Where(v => v.Title.Contains(title));
+
+        public static Video GetByClosestTime(this IEnumerable<Video> videos, TimeSpan duration)
+            => videos?.GetManyByClosestTime(duration).FirstOrDefault();
+
+        public static IEnumerable<Video> GetManyByClosestTime(this IEnumerable<Video> videos, TimeSpan duration)
         {
-            return videos.FirstOrDefault(v => v.Title.Contains(title));
+            var within = 10;
+            var result = videos?.Select(x => new { x, distance = Math.Abs(x.Duration.TotalSeconds - duration.TotalSeconds) })
+              .Where(p => p.distance <= within)
+              .Select(p => p.x);
+
+            return result;
         }
 
-        public static IEnumerable<Video> GetManyByTitle(this IReadOnlyList<Video> videos, string title)
+        private static Video GetByAuthor(this IEnumerable<Video> videos, IEnumerable<string> authors)
+            => videos.GetManyByAuthors(authors).FirstOrDefault();
+
+        private static IEnumerable<Video> GetManyByAuthors(this IEnumerable<Video> videos, IEnumerable<string> authors)
         {
-            return videos.Where(v => v.Title == title);
+            var filteredVideos = new List<Video>();
+
+            // This is ugly and needs to be refactored.
+            foreach (var video in videos)
+            {
+                foreach (var author in authors)
+                {
+                    if (video.Title.Contains(author) && !filteredVideos.Contains(video))
+                    {
+                        filteredVideos.Add(video);
+                    }
+                }
+            }
+
+            return filteredVideos;
         }
 
-        public static Video GetBestMatch(this IReadOnlyList<Video> videos, string title, TimeSpan duration)
+        public static Video GetBestMatch(this IEnumerable<Video> videos, TimeSpan duration,
+            string title, IEnumerable<string> artists, bool appendLyrics = false)
         {
-            var titleResults = videos.Where(v => v.Title == title);
-            var durationResult = titleResults.FirstOrDefault(v => v.Duration.TotalSeconds == duration.TotalSeconds);
-
-            return durationResult;
+            return videos.GetBestMatches(duration, title, artists, appendLyrics).FirstOrDefault();
         }
 
-        public static IEnumerable<Video> GetBestMatches(this IReadOnlyList<Video> videos, string title, TimeSpan duration)
+        public static IEnumerable<Video> GetBestMatches(this IEnumerable<Video> videos, TimeSpan duration, 
+            string title, IEnumerable<string> artists, bool appendLyrics = true)
         {
-            var titleResults = videos.Where(v => v.Title == title);
-            var durationResults = titleResults.Where(v => v.Duration.TotalSeconds == duration.TotalSeconds);
+            var titleResults = videos?.GetManyByTitle(title);
+            if (appendLyrics) { titleResults = titleResults?.GetManyByTitle("lyrics"); }
 
-            return durationResults;
+            var durationResults = titleResults?.GetManyByClosestTime(duration);
+            var artistsResults = durationResults?.GetManyByAuthors(artists);
+
+            return artistsResults;
         }
     }
 }

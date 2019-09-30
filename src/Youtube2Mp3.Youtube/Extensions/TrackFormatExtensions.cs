da1c12b32008abd84353161333d8bf8dd3f60443
Joel Parkinson <joelparkinson@live.co.uk>
2019-10-01 00:57:19 +0100

    Create TrackFormatExtensions.cs
    This extention method is for formatting the query. It simply returns a propperly formatted query based on the options passed into it.

---
+++ src/Youtube2Mp3.Youtube/Extensions/TrackFormatExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using Youtube2Mp3.Core.Entities;
+using Youtube2Mp3.Core.Extensions;
+
+namespace Youtube2Mp3.Youtube.Extensions
+{
+    public static class TrackFormatExtensions
+    {
+        public static string QueryFormat(this Track track, bool shouldUseAuthor, bool shouldUseLyrics)
+        {
+            if (shouldUseAuthor && !shouldUseLyrics) { return $"{track.Authors.First()} - {track.Title}"; }
+            if (shouldUseLyrics && !shouldUseAuthor) { return $"{track.Title} lyrics"; }
+            if (shouldUseAuthor && shouldUseLyrics) { return $"{track.Authors.First()} - {track.Title} lyrics"; }
+            return track.Title;
+        }
+
+        public static string FormatSafeTrackName(this Track track)
+        {
+            var author = track.Authors.FirstOrDefault();
+
+            if (string.IsNullOrEmpty(author))
+                return $"{track.Title}".RemoveIllegalPathCharacters();
+            else
+                return $"{author} - {track.Title}".RemoveIllegalPathCharacters();
+        }
+    }
+}

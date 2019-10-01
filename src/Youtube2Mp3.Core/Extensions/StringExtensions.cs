ce193c82e2d80378d3f45c51bc370efac0ea274f
Joel Parkinson <joelparkinson@live.co.uk>
2019-10-01 01:02:51 +0100

    Simplify FilterClosestTime
    Remove regex and now does what is essentially a replace function across a whole string for any IllegalFileNameChars.

--- src/Youtube2Mp3.Core/Extensions/StringExtensions.cs
+++ src/Youtube2Mp3.Core/Extensions/StringExtensions.cs
@@ -6,10 +6,6 @@ namespace Youtube2Mp3.Core.Extensions
     public static class StringExtensions
     {
         public static string RemoveIllegalPathCharacters(this string path)
-        {
-            string regexSearch = $"{Path.GetInvalidFileNameChars().ToString()}{Path.GetInvalidPathChars().ToString()}";
-            var r = new Regex(string.Format("[{0}]", Regex.Escape(regexSearch)));
-            return r.Replace(path, "");
-        }
+            => string.Join("_", path.Split(Path.GetInvalidFileNameChars()));
     }
 }

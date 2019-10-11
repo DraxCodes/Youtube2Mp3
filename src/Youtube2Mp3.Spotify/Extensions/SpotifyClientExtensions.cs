f63cdc381709cf523f52d5104e16288579203014
TheSecurity <30444943+TheSecurity@users.noreply.github.com>
2019-10-11 07:03:33 +0200

    Refactoring - Return ternary operator, Regex add readonly modifer

--- src/Youtube2Mp3.Spotify/Extensions/SpotifyClientExtensions.cs
+++ src/Youtube2Mp3.Spotify/Extensions/SpotifyClientExtensions.cs
@@ -6,20 +6,21 @@ namespace Youtube2Mp3.Spotify.Extensions
 {
     public static class SpotifyClientExtensions
     {
-        private static Regex spotifyPlaylistIdPattern = new Regex(@"playlist[\/|:](.{22})", RegexOptions.Compiled);
+        private static readonly Regex SpotifyPlaylistIdPattern = new Regex(@"playlist[\/|:](.{22})", RegexOptions.Compiled);
 
         public static FullPlaylist GetPlaylistByUrl(this SpotifyWebAPI api, string url)
         {
             var id = ParseSpotifyIdFromUrl(url);
-            if (id is null) { return null; }
 
-            return api.GetPlaylist(id);
+            return id is null 
+                ? null 
+                : api.GetPlaylist(id);
         }
 
         public static string ParseSpotifyIdFromUrl(string url)
         {
-            if (url is null || !spotifyPlaylistIdPattern.IsMatch(url)) { return null; }
-            return spotifyPlaylistIdPattern.Match(url).Groups[1].Value;
+            if (url is null || !SpotifyPlaylistIdPattern.IsMatch(url)) { return null; }
+            return SpotifyPlaylistIdPattern.Match(url).Groups[1].Value;
         }
     }
 }

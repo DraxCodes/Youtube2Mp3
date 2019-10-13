b58c4cae30901bd8fac5e5b173aabb4f1dc189c3
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-10-13 22:06:59 +0200

    Initial (template) implementation Basic template (tabbed) implementation of a Xamarin.Forms project.

---
+++ src/Youtube2Mp3.Xamarin/Youtube2Mp3.Xamarin/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Youtube2Mp3.Xamarin.Models;
+
+namespace Youtube2Mp3.Xamarin.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}

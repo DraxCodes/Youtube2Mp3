502471ae3a53c24267638db62ea3b910644a643a
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-11-02 11:42:10 +0100

    Add access modifiers

--- src/Youtube2Mp3.Xamarin/Youtube2Mp3.Xamarin/ViewModels/BaseViewModel.cs
+++ src/Youtube2Mp3.Xamarin/Youtube2Mp3.Xamarin/ViewModels/BaseViewModel.cs
@@ -7,15 +7,14 @@ namespace Youtube2Mp3.Xamarin.ViewModels
 {
     public class BaseViewModel : INotifyPropertyChanged
     {
-
-        bool isBusy = false;
+        private bool isBusy = false;
         public bool IsBusy
         {
             get { return isBusy; }
             set { SetProperty(ref isBusy, value); }
         }
 
-        string title = string.Empty;
+        private string title = string.Empty;
         public string Title
         {
             get { return title; }

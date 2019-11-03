3165cee3acce7f6b62c04e858e3fb6c9c7771148
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-11-03 01:47:08 +0100

    Add IThumbnailRepository interface

---
+++ src/Youtube2Mp3.Core/Services/IThumbnailRepository.cs
@@ -0,0 +1,9 @@
+﻿using Youtube2Mp3.Core.Entities;
+
+namespace Youtube2Mp3.Core.Services
+{
+    public interface IThumbnailRepository
+    {
+        string DownloadThumbnailAndGetFilePath(Track track);
+    }
+}

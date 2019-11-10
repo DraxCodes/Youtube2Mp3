bc79b0eb2980394730e2443c6814b89997d0a4ab
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-11-10 14:43:55 +0100

    Implement IOC

---
+++ src/Youtube2Mp3.Xamarin/Youtube2Mp3.Xamarin/IOC/Setup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Youtube2Mp3.IOC;
+using System;
+
+namespace Youtube2Mp3.Xamarin.IOC
+{
+    public static class Setup
+    {
+        public static IServiceProvider Services()
+            => new ServiceCollection()
+            .UseYoutube()
+            .UseSpotify()
+            .BuildServiceProvider();
+    }
+}

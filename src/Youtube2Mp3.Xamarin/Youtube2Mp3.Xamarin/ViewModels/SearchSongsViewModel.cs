5a5b7f4d51fc748b9f383e14909d6dc9a00abaf4
Senne Van Rompaey <vanrompaey.senne1@gmail.com>
2019-11-10 14:44:10 +0100

    Start working on SearchSongsPage

--- src/Youtube2Mp3.Xamarin/Youtube2Mp3.Xamarin/ViewModels/SearchSongsViewModel.cs
+++ src/Youtube2Mp3.Xamarin/Youtube2Mp3.Xamarin/ViewModels/SearchSongsViewModel.cs
@@ -1,23 +1,33 @@
-﻿using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
 using Xamarin.Forms;
+using Youtube2Mp3.Core.Entities;
+using Youtube2Mp3.Core.Services;
 
 namespace Youtube2Mp3.Xamarin.ViewModels
 {
     public class SearchSongsViewModel : BaseViewModel
     {
+        private IStreamRepository _streamRepository;
+
+        public SearchSongsViewModel(IStreamRepository streamRepository)
+        {
+            _streamRepository = streamRepository;
+        }
+
         public Command GetSearchResultsCommand { get; set; }
         public string UserInput { get; set; }
+        public List<Track> Songs
+        {
+            get => GetSearchResults(UserInput).GetAwaiter().GetResult();
+        }
 
         public SearchSongsViewModel()
         {
             GetSearchResultsCommand = new Command(async () => await GetSearchResults(UserInput), () => !IsBusy);
         }
 
-        private async Task GetSearchResults(string query)
-        {
-            // Use Youtube2Mp3 library here
-
-            await Task.CompletedTask;
-        }
+        private async Task<List<Track>> GetSearchResults(string query)
+            => _streamRepository.SearchAsync(query);
     }
 }
